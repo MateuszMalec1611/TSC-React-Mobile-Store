cc72416664f05f417ca9f9c85f012076186f5859
Mateusz Malec <m.mateusz2000@wp.pl>
2021-08-02 18:16:17 +0200

    feat: add loader and fix nav

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,21 +1,33 @@
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { motion } from 'framer-motion';
 import { RootStore } from '@store/Store';
 import pageTransitionFM from '@pages/pageTransition';
+import Button from '@components/Ui/Button/Button';
 import styles from './ProductDetail.module.css';
+import { useEffect } from 'react';
+import { GetProductDetail } from '@store/Services/MobileStore.services';
 interface ParamTypes {
     productName: string;
 }
 
 const ProductDetail: React.FC = () => {
     const { productName } = useParams<ParamTypes>();
+    const dispatch = useDispatch();
     const product = useSelector((state: RootStore) => state.products.productDetail);
+
+    useEffect(() => {
+        dispatch(GetProductDetail(productName));
+    }, [dispatch, productName]);
 
     return (
         <motion.div {...pageTransitionFM} className={styles.productDetail}>
             <h2>{product?.name}</h2>
             <img src={product?.img} alt={productName} />
+            <article>
+                <h4>Price</h4>
+                <p>{product?.price}$</p>
+            </article>
             <article>
                 <h4>Display</h4>
                 <p>{product?.display}</p>
@@ -28,6 +40,7 @@ const ProductDetail: React.FC = () => {
                 <h4>RAM</h4>
                 <p>{product?.ram}</p>
             </article>
+            <Button>Order</Button>
         </motion.div>
     );
 };

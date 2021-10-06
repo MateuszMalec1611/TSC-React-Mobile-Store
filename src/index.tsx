f97dc1cebc68403a93852ed3461dc7cff98fff44
Mateusz Malec <m.mateusz2000@wp.pl>
2021-10-06 23:49:35 +0200

    fix: react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import MobileStore from './MobileStore';
 import store from './store/Store';
 import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <React.StrictMode>
             <Provider store={store}>
                 <MobileStore />
             </Provider>
         </React.StrictMode>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
